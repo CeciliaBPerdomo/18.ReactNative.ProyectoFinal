2765af67c6c3f3ec0db61decdbcc281730912dd0
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2024-03-11 10:59:07 -0300

    Tab Navigation

--- src/navigation/MainNavigation.js
+++ src/navigation/MainNavigation.js
@@ -3,81 +3,18 @@ import { StyleSheet, Text, View } from 'react-native'
 // Navegacion
 import { NavigationContainer } from '@react-navigation/native';
 
-// Bottoms Tabs
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import TabNavigation from './TabNavigation';
 
-import BookStack from './BookStack';
-import CartBooks from '../components/cart/CartBooks';
-import BookOrders from '../components/BookOrders';
-
-import TabBarIcon from '../components/TabBarIcon';
-import CartStack from './CartStack';
-import OrdersStack from './OrdersStack';
-import colors from '../utils/colors';
-
-const Tab = createBottomTabNavigator();
 
 
 const MainNavigation = () => {
     return (
         <>
             <NavigationContainer>
-                <Tab.Navigator
-                    initialRouteName='BookStack'
-                    screenOptions={{
-                        headerShown: false,         // No muestra el encabezado del tab
-                        tabBarShowLabel: false,     // No muestra el texto del tab
-                        tabBarStyle: styles.tabBar,
-                    }}
-                >
-
-                    <Tab.Screen
-                        name="BookStack"
-                        component={BookStack}
-                        options={{
-                            tabBarIcon: ({ focused }) => <TabBarIcon
-                                title="Libros"
-                                icono="home"
-                                focused={focused} />
-                        }}
-                    />
-
-                    <Tab.Screen
-                        name="Cart"
-                        component={CartStack}
-                        options={{
-                            tabBarIcon: ({ focused }) => <TabBarIcon
-                                title="Carrito"
-                                icono="shopping-cart"
-                                focused={focused} />
-                        }}
-                    />
-
-                    <Tab.Screen
-                        name="Orders"
-                        component={OrdersStack}
-                        options={{
-                            tabBarIcon: ({ focused }) => <TabBarIcon
-                                title="Órdenes"
-                                icono="list"
-                                focused={focused} />
-                        }}
-                    />
-
-                </Tab.Navigator>
+                <TabNavigation />
             </NavigationContainer>
         </>
     )
 }
 
 export default MainNavigation
-
-const styles = StyleSheet.create({
-    tabBar: {
-        height: 80,
-        shadowColor: "black",
-        position: "absolute",
-        elevation: 4,
-        backgroundColor: colors.iconos
-    }
-})
---
+++ src/navigation/TabNavigation.js
@@ -0,0 +1,75 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+import colors from '../utils/colors';
+
+// Bottoms Tabs
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import BookStack from './BookStack';
+
+import TabBarIcon from '../components/TabBarIcon';
+import CartStack from './CartStack';
+import OrdersStack from './OrdersStack';
+
+const Tab = createBottomTabNavigator();
+
+const TabNavigation = () => {
+    return (
+        <Tab.Navigator
+            initialRouteName='BookStack'
+            screenOptions={{
+                headerShown: false,         // No muestra el encabezado del tab
+                tabBarShowLabel: false,     // No muestra el texto del tab
+                tabBarStyle: styles.tabBar,
+            }}
+        >
+
+            <Tab.Screen
+                name="BookStack"
+                component={BookStack}
+                options={{
+                    tabBarIcon: ({ focused }) => <TabBarIcon
+                        title="Libros"
+                        icono="home"
+                        focused={focused} />
+                }}
+            />
+
+            <Tab.Screen
+                name="Cart"
+                component={CartStack}
+                options={{
+                    tabBarIcon: ({ focused }) => <TabBarIcon
+                        title="Carrito"
+                        icono="shopping-cart"
+                        focused={focused} />
+                }}
+            />
+
+            <Tab.Screen
+                name="Orders"
+                component={OrdersStack}
+                options={{
+                    tabBarIcon: ({ focused }) => <TabBarIcon
+                        title="Órdenes"
+                        icono="list"
+                        focused={focused} />
+                }}
+            />
+
+        </Tab.Navigator>
+    )
+}
+
+export default TabNavigation
+
+const styles = StyleSheet.create({
+    tabBar: {
+        height: 80,
+        shadowColor: "black",
+        position: "absolute",
+        elevation: 4,
+        backgroundColor: colors.iconos
+    }
+})
